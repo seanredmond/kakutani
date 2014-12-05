6e73aedda0662ab1e376aefdbab48f89d655d86b
Sean Redmond <github-smr@sneakemail.com>
2014-12-05 11:48:24 -0500

    Add ListName#oldest and #newest methods to return Dates

--- lib/kakutani/bestsellers/list_name.rb
+++ lib/kakutani/bestsellers/list_name.rb
@@ -1,6 +1,13 @@
 module Kakutani
   module Bestsellers   
     class ListName < Resource
+      attr_reader :oldest, :newest
+      def initialize(data)
+        super(data)
+        @oldest = Date.strptime(oldest_published_date)
+        @newest = Date.strptime(newest_published_date)
+      end
+
       def self.path
         Bestsellers::path('names')
       end
--- spec/list_name_spec.rb
+++ spec/list_name_spec.rb
@@ -5,6 +5,26 @@ describe Kakutani::Bestsellers::ListName do
     @list = Kakutani::Bestsellers::ListName.new(JSON.parse(LIST_NAMES)['results'].first)
   end
 
+  describe "#oldest" do
+    it "should be a Date" do
+      expect(@list.oldest).to be_an_instance_of Date
+    end
+
+    it "should be the correct date" do
+      expect(@list.oldest).to eq Date.new(2011, 2, 13)
+    end
+  end
+
+  describe "#newest" do
+    it "should be a Date" do
+      expect(@list.newest).to be_an_instance_of Date
+    end
+
+    it "should be the correct date" do
+      expect(@list.newest).to eq Date.new(2014, 12, 7)
+    end
+  end
+
   describe "basic properties" do
     it "has #display_name" do
       expect(@list.display_name).to eq "Combined Print & E-Book Fiction"
@@ -29,8 +49,6 @@ describe Kakutani::Bestsellers::ListName do
     it "has #updated" do
       expect(@list.updated).to eq "WEEKLY"
     end
-
-
   end
 end
 
